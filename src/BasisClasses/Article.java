7cb776bf38c51b95c839dc8c9ccb32ac1c721d69
Francisco Vargas <aliastheblank@gmail.com>
2023-04-02 18:35:39 -0400

    feat: java project created

---
+++ src/BasisClasses/Article.java
@@ -0,0 +1,15 @@
+package BasisClasses;
+
+public class Article {
+
+	private int id;
+	private String author;
+	private String journal;
+	private String title;
+	private int year;
+	private String volume;
+	private int number;
+	private int pages;
+	private String[] keywords;
+	private String doi;
+}
