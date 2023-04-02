56155ffc659e5b00cc9e0779ad8b20c31438d6f5
AliasTheBlank <aliastheblank@gmail.com>
2023-04-02 19:59:14 -0400

    feat: constructors

--- src/BasisClasses/Article.java
+++ src/BasisClasses/Article.java
@@ -12,4 +12,64 @@ public class Article {
 	private int pages;
 	private String[] keywords;
 	private String doi;
+	
+	/**
+	 * @param id
+	 * @param author
+	 * @param journal
+	 * @param title
+	 * @param year
+	 * @param volume
+	 * @param number
+	 * @param pages
+	 * @param keywords
+	 * @param doi
+	 */
+	public Article(int id, String author, String journal, String title, int year, String volume, int number, int pages,
+			String doi, String ... keywords) {
+		this.id = id;
+		this.author = author;
+		this.journal = journal;
+		this.title = title;
+		this.year = year;
+		this.volume = volume;
+		this.number = number;
+		this.pages = pages;
+		this.keywords = new String[keywords.length];
+		
+		for (int i = 0; i < keywords.length; i++) 
+			this.keywords[i] = keywords[i];
+
+		
+		this.doi = doi;
+	}
+	
+
+	/**
+	 * @param id
+	 * @param author
+	 * @param journal
+	 * @param title
+	 * @param year
+	 * @param volume
+	 * @param number
+	 * @param pages
+	 * @param keywords
+	 * @param doi
+	 */
+	public Article(int id, String author, String journal, String title, int year, String volume, int number, int pages,
+			String[] keywords, String doi) {
+		this.id = id;
+		this.author = author;
+		this.journal = journal;
+		this.title = title;
+		this.year = year;
+		this.volume = volume;
+		this.number = number;
+		this.pages = pages;
+		this.keywords = keywords;
+		this.doi = doi;
+	}
+	
+	
 }
