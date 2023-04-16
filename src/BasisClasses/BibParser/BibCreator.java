e52c92cffe0d74bae1634e1726296a2ab84720c0
AliasTheBlank <aliastheblank@gmail.com>
2023-04-16 18:16:34 -0400

    feat: create files after being able to read them

--- src/BasisClasses/BibParser/BibCreator.java
+++ src/BasisClasses/BibParser/BibCreator.java
@@ -1,7 +1,10 @@
 package BasisClasses.BibParser;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,6 +28,8 @@ import BasisClasses.BibParser.Tokens.TokenType;
 import BasisClasses.BibParser.Tokens.ValueToken;
 
 public class BibCreator {
+	
+	static HashMap<Integer, List<PrintWriter>> pendingFiles;
 
 	public static void main(String[] args) {
 		
@@ -44,13 +49,16 @@ public class BibCreator {
 
 		StringBuilder[] sb = new StringBuilder[myUserProvidedPaths.length];
 
+		int numberOfInvalidFiles = 0;
 
 		for(int i = 0; i < myUserProvidedPaths.length; i++) {
 		  Scanner scanner;
 		  try {
 		    scanner = new Scanner(new FileInputStream(myUserProvidedPaths[i]));
 		    sb[i] = catchData(scanner);
+		    
 		  } catch (Exception e) {
+			  numberOfInvalidFiles++;
 		    System.out.printf("""
 		    		Could not open input file %s for reading.
 		    		
@@ -59,33 +67,83 @@ public class BibCreator {
 		  }
 		}
 		
+		if (numberOfInvalidFiles > 0) {
+			System.out.println(String.format("A total of %d files were invalid, and could not be processed. All other %d \"Valid\" files have been created", numberOfInvalidFiles, myUserProvidedPaths.length - numberOfInvalidFiles));
+		}
+		
+		pendingFiles = new HashMap<Integer, List<PrintWriter>>();
+		for (int i = 0; i < myUserProvidedPaths.length; i++) {
+			pendingFiles.put((i + 1), new ArrayList<PrintWriter>());
+			List<PrintWriter> list = pendingFiles.get(i + 1);
+            try {
+            	list.add(new PrintWriter(String.format("IEEE%d.json", i + 1))); 
+				list.add(new PrintWriter(String.format("ACM%d.json", i + 1)));
+				list.add(new PrintWriter(String.format("NJ%d.json", i + 1)));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			} 
+		}
+		
 		for (int i = 0; i < myUserProvidedPaths.length; i++) {
 			try {
-				processFilesForValidation(new String(sb[i]), i, myUserProvidedPaths[i]);
+				processFilesForValidation(new String(sb[i]), i + 1, myUserProvidedPaths[i]);
 			}
 			catch (Exception e) {
 				// TODO: handle exception
 				System.out.println(e.getMessage());
 			}
 		}
+		
+		int tries = 0;
+		while (tries < 3) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Pleasse enter the name of one of the files that you need to review:");
+			String fileToReview = scanner.next();
+			System.out.println();
+			scanner.close();
+			
+			FileReader fr;
+			BufferedReader bf;
+			try {
+				fr = new FileReader(fileToReview);
+				bf = new BufferedReader(fr);
+				while (bf.ready()) {
+					System.out.print((char)bf.read());
+				}
+				break;
+			} catch (Exception e) {
+				tries++;
+				if (tries == 2) {
+					System.out.println("Could not open input file agail. File does not exist or could not be created!");
+					System.out.println("Sorry! I am unable to display your desired files! Program will exit!");
+					return;
+				}
+				System.out.println("Could not open input file. File does not exist; possibly it could not be created!");
+			}
+			
+		}
 	}
 	
 	
 	//TODO
-	public static void processFilesForValidation(String file, int number, String fileName) throws Exception {
+	public static void processFilesForValidation(String fileInformation, int number, String fileName) throws Exception {
 		
+		String tempToken = null;
+		String id = null;
+		String interimKey = null;
 		String delim = "{},=\n";
-        StringTokenizer st = new StringTokenizer(file, delim, true);
+		
+		String[] files = {String.format("IEEE%d.json", number), String.format("ACM%d.json", number), String.format("NJ%d.json", number)};
+		
+        StringTokenizer st = new StringTokenizer(fileInformation, delim, true);
 
         Stack<IToken> tokenStack = new Stack<IToken>();
         Stack<Boolean> bracketStack = new Stack<Boolean>();
         
         HashMap<String, HashMap<String, String>> rawData = new HashMap<String, HashMap<String, String>>();
         
-        String tempToken = null;
         
-        String id = null;
-        String interimKey = null;
         
         while (st.hasMoreTokens() || tempToken != null) {
         	String nextToken = (tempToken != null ? tempToken : st.nextToken()).strip();
@@ -122,7 +180,17 @@ public class BibCreator {
         	IToken closeBracket = CloseBracketToken.match(nextToken, bracketStack);
         	if (closeBracket != null) {
         		
-					if (tokenStack.peek().GetTokenType() == TokenType.OpenBracket)
+					if (tokenStack.peek().GetTokenType() == TokenType.OpenBracket) {
+						
+						List<PrintWriter> list = pendingFiles.get(number);
+						for (PrintWriter printWriter : list) {
+							printWriter.close();
+						}
+				        for (String string : files) {
+							File file = new File(string);
+							file.delete();
+						}
+						
 						 throw new FileInvalidException(String.format("""
 						 		Error: Detected Empty Field!
 						 		============================
@@ -130,6 +198,7 @@ public class BibCreator {
 						 		Problem detected with input file: %s
 						 		File is Invalid: Field "%s" is Empty. Processing stopped at this point. Other empty fields may be present as well!
 						 		""", fileName, interimKey));
+					}
 					
         		bracketStack.pop();
         		tokenStack.push(closeBracket);
@@ -228,7 +297,6 @@ public class BibCreator {
         			ArticleMap.get("issn")));
         });
         
-        String[] files = {String.format("IEEE%d.json", number + 1), String.format("ACM%d.json", number + 1), String.format("NJ%d.json", number + 1)};
         PrintWriter pw1 = new PrintWriter(files[0]);
         
         for (Article article : listOfArticles) {
